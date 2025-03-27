3021c2a21306f7bc86cd44959588ad79863e4d08
uzomao <chidumaga@gmail.com>
2025-03-27 17:42:58 +0100

    adds site metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,39 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "Portal",
-  description: "Creative site templates for artists",
+  title: "Portal - Digital Playgrounds for Creative Communities",
+  description: "Create interactive digital experiences to tell your story and connect with your audience",
+  keywords: "digital playgrounds, creatives, interactive experiences, storytelling, audience engagement, community, connection",
+  author: "Uzoma Studio",
+  openGraph: {
+    title: "Portal - Digital Playgrounds for Creative Communities",
+    description: "Create interactive digital experiences to tell your story and connect with your audience",
+    url: "https://portal8.site",
+    site_name: "Portal",
+    images: [
+      {
+        url: "https://portal8.site/og-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Portal - Digital Playgrounds for Creative Communities",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Portal - Digital Playgrounds for Creative Communities",
+    description: "Create interactive digital experiences to tell your story and connect with your audience",
+    images: [
+      {
+        url: "https://portal8.site/og-image.jpg",
+        alt: "Portal - Digital Playgrounds for Creative Communities",
+      },
+    ],
+  },
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  },
 };
 
 export default function RootLayout({ children }) {
